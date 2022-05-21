cf9f9aa09599a9f52d963f195f61133b8a71e50d
Adriano Sousa <adriano.sousa.aas@gmail.com>
2022-05-21 13:44:03 -0300

    Funcionando
    fucionando

---
+++ src/main/java/com/project/professor/allocation/adriano/ProfessorAllocationAdrianoApplication.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation.adriano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationAdrianoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationAdrianoApplication.class, args);
+	}
+
+}
